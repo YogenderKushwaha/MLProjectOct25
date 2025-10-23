9560a872a431958da14bdd2dd77d8b06fcb593cc
Yogender Kushwaha <ykushwaha20@gmail.com>
2025-10-23 23:36:43 +0530

    adding app.py

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -41,14 +41,14 @@ class DataTransformation:
             num_pipeline = Pipeline(
                 steps= [
                 ("imputer", SimpleImputer(strategy="median")),
-                ("scaler", StandardScaler( with_mean= False))
+                ("scaler", StandardScaler())
                 ])
 
             cat_pipeline = Pipeline(
 
                 steps = [
                 ("imputer", SimpleImputer(strategy = 'most_frequent')),
-                ("one_hot_encoder", OneHotEncoder()),
+                ("one_hot_encoder", OneHotEncoder(handle_unknown='ignore', sparse_output=True)),
                 ("scaler", StandardScaler(with_mean=False))
                 ]
             
